5df7ab62eb8fb8e0dc31b204a0086336a1a7fb67
Daniel Beck <dlc.beck@gmail.com>
2021-08-18 10:05:06 -0700

    adds functionality for assoc toys and cats

--- main_app/models.py
+++ main_app/models.py
@@ -23,6 +23,8 @@ class Cat(models.Model):
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
+  # Add the M:M relationaship
+  toys = models.ManyToManyField(Toy)
 
   def __str__(self):
     return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -28,11 +28,16 @@ def cats_index(request):
 
 def cats_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
+  # Get the toy the cat doesn't have
+  toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
   # instantiate FeedingForm to be rendered in the template
   feeding_form = FeedingForm()
   return render(request, 'cats/detail.html', {
     # pass the cat and feeding_form as context
-    'cat': cat, 'feeding_form': feeding_form
+    'cat': cat,
+    'feeding_form': feeding_form,
+    # Add the toys to be displayed
+    'toys': toys_cat_doesnt_have
   })
 
 def add_feeding(request, cat_id):
@@ -64,3 +69,8 @@ class ToyUpdate(UpdateView):
 class ToyDelete(DeleteView):
   model = Toy
   success_url = '/toys/'
+
+def assoc_toy(request, cat_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole object
+  Cat.objects.get(id=cat_id).toys.add(toy_id)
+  return redirect('detail', cat_id=cat_id)
