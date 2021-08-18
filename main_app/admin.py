633cfe5413a77980ecb42478343ef654f8667291
Daniel Beck <dlc.beck@gmail.com>
2021-08-18 08:46:37 -0700

    adds toy CRUD and updates to starter-code for many to many lesson

--- main_app/admin.py
+++ main_app/admin.py
@@ -3,5 +3,4 @@ from .models import Cat, Feeding
 
 # Register your models here.
 admin.site.register(Cat)
-# Register the feeding model
 admin.site.register(Feeding)
--- main_app/forms.py
+++ main_app/forms.py
@@ -1,10 +1,8 @@
-from django.db.models.base import Model
-from django.forms import ModelForm, fields
+from django.forms import ModelForm
 from .models import Feeding
 
-# create a new class called FeedingForm 
-# that extends the ModelForm class
 class FeedingForm(ModelForm):
-    class Meta:
-        model = Feeding
-        fields = ['date', 'meal']
+  class Meta:
+    model = Feeding
+    fields = ['date', 'meal']
+
